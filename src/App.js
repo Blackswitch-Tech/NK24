919476a2169392de35bb6694333da8a098f1aea3
TOM SABU <tomsabu0000@gmail.com>
2024-02-06 06:39:03 +0530

    Navbar --updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
-//import { Navbar } from "./components/Navbar";
+import  Navbar  from "./components/Navbar"; 
 import { RouterPaths } from "./components/router";
 import useReady from "./components/useReady";
 import { Loader } from "./components/loader";
@@ -17,7 +17,7 @@ function App() {
         <Loader />
       ) : (
         <Router>
-          
+          <Navbar/>
           <RouterPaths />
         </Router>
       )}
