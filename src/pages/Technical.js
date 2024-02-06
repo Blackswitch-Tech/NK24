d302d812f077279df0d1750b8c5ecba8f9247092
fredysomy <fredysomy@gmail.com>
2024-02-06 10:45:22 +0530

    Added dropdown select and search

--- src/pages/Technical.js
+++ src/pages/Technical.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+
 
 const Technical = () => {
-    // Your component logic goes here
-
-    return (
-        <div className="w-full ">
+  // Your component logic goes here
+  const [loaded, setLoaded] = useState(false);
+  const [search, setSearch] = useState("");
+  const options = [
+    { value: "Dance", label: "Dance" },
+    { value: "Music", label: "Music" },
+    { value: "Art", label: "Art" },
+    { value: "Drama", label: "Drama" },
+    { value: "Literature", label: "Literature" },
+    { value: "Craft", label: "Craft" },
+  ];
+  return (
+    <div className="w-full ">
       <div className="justify-center align-middle flex">
-        <h1 className="text-white font-libre text-7xl mt-8">Cultural Events</h1>
+        <h1 className="text-white font-libre text-7xl mt-8">Technical Events</h1>
       </div>
       <div className="flex flex-row justify-center mx-10">
         <div class="max-w-md mx-auto w-full mt-10">
@@ -29,19 +40,31 @@ const Technical = () => {
             </div>
 
             <input
-              class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+              class="peer h-full w-full outline-none text-sm text-black pr-2 placeholder:text-black"
               type="text"
               id="search"
               placeholder="Search Events"
+              onChange={(e) => setSearch(e.target.value)}
             />
           </div>
-
+          <div className="mt-10">
+            <div class="relative h-10">
+              <select onChange={(item)=>{console.log(item.target.value)}} class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-white outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
+                {options.map((option) => (
+                  <option value={option.value}>{option.label}</option>
+                ))}
+              </select>
+              <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                All
+              </label>
+            </div>
+          </div>
         </div>
-        
+
         <div></div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Technical;
