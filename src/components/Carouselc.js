88aab19239bf3efe082f2b8e8020fdcbd793b5f1
Allen Bastian Joy <allenbastianjoy@Allen-Bastians-MBP.local>
2024-02-07 14:17:43 +0530

    added

---
+++ src/components/Carouselc.js
@@ -0,0 +1,43 @@
+export default function Carouselc()
+{
+    return(
+        <div className="carousel carousel-center p-4 space-x-4 bg-black rounded-box">
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Neo%20Graffiti.jpg?alt=media&token=50cc9156-5218-443c-96cc-6a12cbe3218a" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Artle.jpg?alt=media&token=f5e0fc71-88c4-47a5-945c-537f48282b4f" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Pencil%20Mania.jpg?alt=media&token=edf839e6-b381-4a91-8e1f-74ae52f4aa1a" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Double%20Trouble.jpg?alt=media&token=d4a489b5-c967-4485-8cd8-b7fcbc751e95" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Duo%20Dance.jpg?alt=media&token=93c5019d-651a-4140-a6ae-d538cf098378" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Sync%20Step1.jpg?alt=media&token=a5e09542-63aa-4d30-821a-4d43a22108ac" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
+        </div>
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Pencil%20Mania.jpg?alt=media&token=edf839e6-b381-4a91-8e1f-74ae52f4aa1a" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Double%20Trouble.jpg?alt=media&token=d4a489b5-c967-4485-8cd8-b7fcbc751e95" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/Duo%20Dance.jpg?alt=media&token=93c5019d-651a-4140-a6ae-d538cf098378" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/COUNTY%20CRICKET.png?alt=media&token=aa7d8e74-97af-4d47-a0a1-fbd0c427a8c9" className="rounded-box" />
+        </div> 
+        <div className="carousel-item relative h-64 w-74">
+          <img src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/BASKETBALL.png?alt=media&token=38ca6811-60c7-4c6f-a5df-d7085d535220" className="rounded-box" />
+        </div>
+      </div>
+    )
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,27 +1,25 @@
 import DefaultAccordion from "../components/DefaultAccordion";
+import Carouselc from "../components/Carouselc";
 
 export default function Homepage() {
   return (
     <div className="main-container">
       <div
         className="upper-section flex h-screen bg-no-repeat bg-cover bg-center"
-        style={{ backgroundImage: `url(${require("./upper.jpeg")})` }}
-      >
+        style={{ backgroundImage: `url(${require("./upper.jpeg")})` }}>
+
         <div className="w-full flex  backdrop-brightness-40">
           <div className="flex w-full h-fit max-w-full justify-center mt-48 ">
             <img
               className="object-contain w-2/3 sm:w-2/3 lg:w-1/4 backdrop-brightness-40 "
               src="https://firebasestorage.googleapis.com/v0/b/sampkle.appspot.com/o/date.png?alt=media&token=a46e48d2-b337-4c60-942e-70ba28cd05c4"
-              alt="Descriptive Alt Text"
-            />
+              alt="Descriptive Alt Text" />
             
           </div>
         </div>
       </div>
-      <div
-        className="middle-section flex h-screen bg-no-repeat bg-cover bg-center opacity-30"
-        
-      >
+      <div className="middle-section">
+                <Carouselc/>
         <div className="w-full h-full flex  justify-left items-center">
           <div className="text-white text-4xl w-1/2 text-center"></div>
         </div>
