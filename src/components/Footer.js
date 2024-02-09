af6cade5965eacac7744d542464fc7c5b9b90b02
AllenBastian <reachallenbastian@gmail.com>
2024-02-09 21:45:11 +0530

    footer modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,47 @@
-import React from "react";
-
+import { Typography } from "@material-tailwind/react";
+ 
 export default function Footer() {
-    return (
-        
-
-<footer class="bg-black rounded-lg shadow m-4 mb-0">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Nakshatra</span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> <a href="https://flowbite.com/" class="hover:underline">Made with Love by Website team</a></span>
-    </div>
-</footer>
-    );
+  return (
+    <footer className="w-full bg-black p-8 font-pop">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-black  text-center md:justify-between">
+        <img src="https://docs.material-tailwind.com/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
+        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="white"
+              className="font-normal transition-colors hover:text- focus:text-blue-500"
+            >
+              About Us
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="White"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              License
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="white"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              Contact Us
+            </Typography>
+          </li>
+        </ul>
+      </div>
+      <hr className="my-8 border-blue-gray-50" />
+      <Typography color="gray" className="text-center font-normal">
+        &copy; 2024 NAKSHATRA Website team
+      </Typography>
+    </footer>
+  );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -76,8 +76,8 @@ export default function Homepage() {
       <div className="container mx-auto lg:mx-4 px-4 py-8  bg-cover bg-center">
       <div className="flex flex-wrap items-center">
         <div className="w-full sm:w-3/4 md:w-1/2 px-2 sm:px-4 md:order-first">
-          <h2 className="text-2xl sm:text-3xl font-bold font-pop mb-4">About Us</h2>
-          <p className="text-base sm:text-lg font-pop">
+          <h2 className="text-2xl sm:text-3xl font-bold text-white font-pop mb-4">About Us</h2>
+          <p className="text-base sm:text-lg text-white font-pop">
           Saintgits College of Engineering presents NAKSHATRA 2023, the annual techno-cultural fest. It is hosted every year to recognise fledging engineers who have a flair of technical expertise and artistry. It is one of the most eminent fests with more than 4000 entrants from across the nation. This extravaganza unfolds a two-day mega event that transports you into a new realm. Get enraptured in performances by budding engineers and artists that will leave you spellbound. Join in to be a part of our exquisite fest, NAKSHATRA 2023.
           </p>
         </div>
@@ -90,7 +90,7 @@ export default function Homepage() {
     </div>
 
       <div className="end-section mx-8">
-        <div className="text-red-800 font-extrabold text-5xl font-pop">FAQ</div>
+        <div className="text-white-800 font-extrabold text-5xl font-pop">FAQ</div>
         <DefaultAccordion />
       </div>
     </div>
