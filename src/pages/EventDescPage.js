29bf4a4e2713f349d5553cbdf5a540c7aebb05b9
AllenBastian <reachallenbastian@gmail.com>
2024-02-13 03:53:56 +0530

    design done

--- src/pages/EventDescPage.js
+++ src/pages/EventDescPage.js
@@ -1,11 +1,12 @@
 // src/components/EventPage.js
 import React, { useEffect, useState } from "react";
-import { MdPhone } from 'react-icons/md'
-import { MdOutlineEmojiEvents as TrophyIcon } from 'react-icons/md';
+import { MdPhone } from "react-icons/md";
+import { MdOutlineEmojiEvents as TrophyIcon } from "react-icons/md";
 import { Input } from "@material-tailwind/react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 import { redirect, useParams } from "react-router-dom";
-import { Dialogbox, LongDialog } from '../components/Dialogbox';
+import { Dialogbox, LongDialog } from "../components/Dialogbox";
 import {
   collection,
   query,
@@ -34,13 +35,12 @@ import { displayRazorpay } from "../razorpay/razorpay";
 const EventPage = () => {
   const [eventData, setEventData] = useState(null);
   const { id } = useParams();
-  const [count,setCount] = useState(0);
+
+  const [newMemberName, setNewMemberName] = useState("");
   const [refCode, setRefcode] = useState(null);
-  const [min,setMin] = useState(null);
-  const [max,setMax] = useState(null);
-  const [team, setTeam] = useState(null);
-  const [currentUser, setCurrentUser] = useState(null);
-  const [userData,setUserData] = useState(null);
+  const [team, setTeam] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
+  const [userData, setUserData] = useState(null);
   const [needSignUp, setNeedSignUp] = useState(true);
   const nav = useNavigate();
   const [open, setOpen] = React.useState(false);
@@ -49,20 +49,17 @@ const EventPage = () => {
 
   useEffect(() => {
     const ref = localStorage.getItem("refcode");
-    if(ref!=="undefined")
-      setRefcode(ref)
+    if (ref !== "undefined") setRefcode(ref);
 
     const fetchEventData = async () => {
       const q = query(collection(db, "events"), where("id", "==", id));
       const querySnapshot = await getDocs(q);
-      console.log(querySnapshot)
-  
+      console.log(querySnapshot);
+
       if (!querySnapshot.empty) {
         const docData = querySnapshot.docs[0].data();
 
-         
         setEventData(docData);
-
       } else {
         console.log("No such event!");
       }
@@ -74,22 +71,20 @@ const EventPage = () => {
       setCurrentUser(user);
       // Hide loader once the auth state is determined
     });
-    
+
     if (currentUser) {
       console.log(currentUser);
       getUserByEmail(currentUser.email).then((data) => {
-
         if (data) {
           setNeedSignUp(false);
         } else {
           setNeedSignUp(true);
-        
         }
       });
     }
     // Cleanup subscription on unmount
     return () => unsubscribe();
-  }, [id,currentUser]);
+  }, [id, currentUser]);
 
   const handleSignIn = () => {
     signInWithPopup(auth, provider)
@@ -106,38 +101,52 @@ const EventPage = () => {
         console.log(error.message);
       });
   };
+  console.log(
+    team.length,
+    eventData?.min,
+    eventData?.max,
+    eventData?.type,
+    currentUser?.email
+  );
 
   const proceedToPay = async () => {
-        if((count > eventData.min && count < eventData.max)||eventData.type === "single"||eventData.type === "Single")
-        {
-          getUserByEmail(currentUser.email).then((userData) => {
-            const token = {
+    if (
+      (team.length >= Number(eventData.min) &&
+        team.length <= Number(eventData.max)) ||
+        eventData.type.toLowerCase() === "single" 
+    ) {
+      getUserByEmail(currentUser.email).then((userData) => {
+        const token = {
+          uid: userData.id,
+          nkid: userData.NKID,
+          username: userData.name,
+          amount: eventData.regfee,
+          eventid: eventData.id,
+          eventname: eventData.name,
+        };
+        displayRazorpay(token);
+      });
+    } else {
+      alert(
+        `number of participants are required between ${eventData.min} and ${eventData.max}`
+      );
+    }
+  };
 
-              uid: userData.id,
-              nkid:userData.NKID,
-              username: userData.name,
-              amount: eventData.regfee,
-              eventid: eventData.id,
-              eventname: eventData.name,
-              
-            
-            };
-            displayRazorpay(token)
-        })
-        }
-        else
-        {
-          alert(`number of participants are required between ${eventData.min} and ${eventData.max}`)
-        }
-    
-}
+  const handleRefCodeChange = (event) => {
+    setRefcode(event.target.value);
+  };
 
-const handleRefCodeChange = (event) => {
-  setRefcode(event.target.value);
-};
+  const addTeamMember = () => {
+    if (newMemberName.trim()) {
+      setTeam([...team, newMemberName.trim()]);
+      setNewMemberName("");
+    }
+  };
 
-
-
+  const deleteTeamMember = (index) => {
+    setTeam(team.filter((_, i) => i !== index));
+  };
 
   if (!eventData) return <div className="text-center p-10">Loading...</div>;
 
@@ -158,14 +167,18 @@ const handleRefCodeChange = (event) => {
           <h1 className="text-3xl text-white font-pop font-bold text-center mb-4">
             {eventData.name}
           </h1>
-          <p className="text-lg mb-4 font-pop">{eventData.description}</p>
+          <p className="text-lg mb-4 font-pop ">{eventData.description}</p>
           <div className="mb-6">
-            <h2 className="text-2xl font-semibold text-white font-pop mb-2">Details</h2>
+            <h2 className="text-2xl font-semibold text-white font-pop mb-2 underline">
+              Details
+            </h2>
             <p className="mb-1 font-pop">
-              Date: <span className="font-medium font-pop">{eventData.date}</span>
+              Date:{" "}
+              <span className="font-medium font-pop ">{eventData.date}</span>
             </p>
             <p className="mb-1 font-pop">
-              Time: <span className="font-medium font-pop">{eventData.time}</span>
+              Time:{" "}
+              <span className="font-medium font-pop u">{eventData.time}</span>
             </p>
             <p className="font-pop">
               Category:{" "}
@@ -175,59 +188,95 @@ const handleRefCodeChange = (event) => {
             </p>
           </div>
           <div className="mb-6">
-            <h2 className="text-2xl text-white font-semibold mb-2 font-pop">Organizers</h2>
+            <h2 className="text-2xl text-white font-semibold mb-2 font-pop underline">
+              Organizers
+            </h2>
             {eventData.people.map((person, index) => (
               <div key={index} className="mb-2">
                 <p>
                   <span className="font-medium font-pop">
-                    {person.title} : {person.name}, <MdPhone className="inline text-white"/> {person.phno}
+                    {person.title} : {person.name},{" "}
+                    <MdPhone className="inline text-white" /> {person.phno}
                   </span>
                 </p>
               </div>
             ))}
           </div>
           <div className="mb-6">
-            <h2 className="text-2xl font-semibol font-pop text-white mb-2">Prizes</h2>
+            <h2 className="text-2xl font-semibol font-pop text-white mb-2 underline">
+              Prizes
+            </h2>
             {eventData.prizes.map((prize, index) => (
               <div key={index} className="mb-2">
                 <p>
-                          <span className="font-medium font-pop">
-              {prize.title === "1st" && <TrophyIcon className="text-yellow-500 inline" size={32} />}
-              {prize.title === "2nd" && <TrophyIcon className="text-gray-400 inline" size={32} />}
-              {prize.title === "3rd" && <TrophyIcon className="text-orange-400 inline" size={32} />}
-              {prize.title !== "1st" && prize.title !== "2nd" && prize.title !== "3rd" && prize.title}{" "}
-               {prize.amt}
-            </span>
-                          </p>
+                  <span className="font-medium font-pop">
+                    {prize.title === "1st" && (
+                      <TrophyIcon
+                        className="text-yellow-500 inline"
+                        size={32}
+                      />
+                    )}
+                    {prize.title === "2nd" && (
+                      <TrophyIcon className="text-gray-400 inline" size={32} />
+                    )}
+                    {prize.title === "3rd" && (
+                      <TrophyIcon
+                        className="text-orange-400 inline"
+                        size={32}
+                      />
+                    )}
+                    {prize.title !== "1st" &&
+                      prize.title !== "2nd" &&
+                      prize.title !== "3rd" &&
+                      prize.title}{" "}
+                    {prize.amt}
+                  </span>
+                </p>
               </div>
             ))}
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-white font-pop mb-2">Rules</h2>
-            <Button className=" hover:bg-green-500 py-4 px-2 font-pop" onClick={handleOpen}>View Rules</Button>
-      <Dialog open={open} handler={handleOpen}>
-        <DialogHeader>Event Rules</DialogHeader>
-        <DialogBody className="h-[42rem] overflow-scroll">
-          <Typography className="font-normal">
-          <ul className="list-disc pl-5">
-              {eventData.rules.map((rule, index) => (
-                <li key={index}>{rule}</li>
-              ))}
-            </ul>
-          </Typography>
-        </DialogBody>
-        <DialogFooter className="space-x-2">
-          <Button className="font-pop" variant="text" color="blue-gray" onClick={handleOpen}>
-            Cancel
-          </Button>
-          <Button className="font-pop" variant="gradient" color="green" onClick={handleOpen}>
-            Confirm
-          </Button>
-        </DialogFooter>
-      </Dialog>
+            <Button
+              className=" hover:bg-green-500 py-4 px-2 font-pop"
+              onClick={handleOpen}
+            >
+              View Rules
+            </Button>
+            <Dialog open={open} handler={handleOpen}>
+              <DialogHeader>Event Rules</DialogHeader>
+              <DialogBody className="h-[42rem] overflow-scroll">
+                <Typography className="font-normal">
+                  <ul className="list-disc pl-5">
+                    {eventData.rules.map((rule, index) => (
+                      <li key={index}>{rule}</li>
+                    ))}
+                  </ul>
+                </Typography>
+              </DialogBody>
+              <DialogFooter className="space-x-2">
+                <Button
+                  className="font-pop"
+                  variant="text"
+                  color="blue-gray"
+                  onClick={handleOpen}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  className="font-pop"
+                  variant="gradient"
+                  color="green"
+                  onClick={handleOpen}
+                >
+                  Confirm
+                </Button>
+              </DialogFooter>
+            </Dialog>
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-white font-pop mb-2">Click to Register</h2>
+            <h2 className="text-2xl font-semibold text-white font-pop mb-2 underline">
+              Register
+            </h2>
             <div className="flex flex-col md:flex-row gap-4">
               {currentUser ? (
                 <>
@@ -243,60 +292,78 @@ const handleRefCodeChange = (event) => {
                       </button>
                     </>
                   ) : (
-                    <>
-                    {
-                  eventData && eventData.type === "team" && (
-                    <>
-                      <button
-                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4"
-                        onClick={() => setCount(count + 1)}
-                      >
-                        Increase
-                      </button>
-                      <button
-                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4"
-                        onClick={() => setCount((prevCount) => prevCount > 0 ? prevCount - 1 : 0)}
-                      >
-                        Decrease
-                      </button>
-                      <h1>{count}</h1>
-                   </>
+                    <div className="flex flex-col">
+                      {eventData &&  eventData.type.toLowerCase() == "team" && (
+                        <div className="mt-5 flex flex-col  justify-center mx-0 gap-3">
+                          <div className="w-full text-left">
+                            <h1 className="font-pop text-white">
+                              Add team member
+                            </h1>
+                          </div>
+                          <div className="flex gap-2 ">
+                            <input
+                              type="text"
+                              value={newMemberName}
+                              onChange={(e) => setNewMemberName(e.target.value)}
+                              className="text-white font-pop py-2 px-4 w-72 rounded"
+                              placeholder="Enter team member's name"
+                            />
+                            <button
+                              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                              onClick={addTeamMember}
+                            >
+                              <AiOutlinePlus />
+                            </button>
+                          </div>
+                          <div className="w-full flex flex-col items-start gap-2">
+                            {" "}
+                            {/* Adjust for alignment */}
+                            {team.map((member, index) => (
+                              <div
+                                key={index}
+                                className="flex items-center justify-start gap-4 w-full"
+                              >
+                                <button
+                                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                                  onClick={() => deleteTeamMember(index)}
+                                >
+                                  <AiOutlineMinus />
+                                </button>
+                                <div className="text-xl font-pop text-white flex-1">
+                                  {member}
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      )}
 
-                  )
-                }
+                      <div className="w-full flex flex-col  justify-center gap-6">
+                        <div className="font-pop text-white mt-7">REFERRAL CODE</div>
+                        <div className="flex w-72 flex-col gap-6">
+                          <input
+                            type="text"
+                            value={refCode}
+                            onChange={handleRefCodeChange}
+                            className="text-white font-pop py-2 px-4 rounded"
+                            placeholder="Enter referral code"
+                          />
+                        </div>
+                      </div>
 
-                                 <div>
-                                    <div className="font-pop text-white">REFERRAL CODE</div>
-                                    <div className="flex w-72 flex-col gap-6">
-                                      <input 
-                                        type="text"
-                                        color="blue" 
-                                        value={refCode}
-                                        onChange={handleRefCodeChange} 
-                                        readOnly={!!refCode} 
-                                        className="input-class" 
-                                      />
-                                    </div>
-                                  </div>
-                                        
-                                        
                       <button
-                        className="bg-blue-500 hover:bg-blue-700 font-pop text-white font-bold py-2 px-4 rounded"
+                        className="bg-blue-500 mt-3 hover:bg-blue-700 font-pop w-72 text-white font-bold py-2 px-4 rounded"
                         onClick={() => {
-                          proceedToPay()
-                        
+                          proceedToPay();
                         }}
                       >
                         REGISTER
                       </button>
-                    </>
+                    </div>
                   )}
-
-                      
                 </>
               ) : (
                 <>
-                  
                   <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={() => {
