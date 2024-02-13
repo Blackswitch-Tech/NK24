580a32b3bdb451157bd6fcba8225a24b78f7ba77
Basil Rari <basilrari@gmail.com>
2024-02-13 17:36:54 +0530

    Loader Adder

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles.css"
+import loading from '../assets/NKanimation.gif'
 
 export const Loader = () => {
   return (
     <div id="loadingBox" className="z-10">
       <img
         id="loading"
-        src="https://firebasestorage.googleapis.com/v0/b/nk23-a5689.appspot.com/o/loadnew.gif?alt=media&token=19eedb4e-5a84-4b6b-bd83-39dbf847e425"
+        src={loading}
         alt="loading"
       />
     </div>
