07b64c1fab28620d541245d28a19a86d9e6f424e
fredysomy <fredysomy@gmail.com>
2024-02-16 21:19:55 +0530

    The end of master edging

--- src/pages/EventDescPage.js
+++ src/pages/EventDescPage.js
@@ -413,6 +413,25 @@ const EventPage = () => {
                                 before attempting another payment.
                               </span>
                             </div>
+                            <div
+                              class=" border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                              role="alert"
+                            >
+                              <strong class="font-bold font-pop">
+                                Disclaimer:
+                              </strong>
+                              <span class="block sm:inline text-xs text-pop">
+                                {" "}
+                                If you have manually closed and open the Nakshatra webiste during payment, there is some chance that nothin happens, if so do to {" "}
+                                <a
+                                  className="underline text-red-200"
+                                  href="/dashboard"
+                                >
+                                  dashboard
+                                </a>{" "}
+                                before attempting another payment or contacting.
+                              </span>
+                            </div>
                           </>
                         ) : (
                           <></>
